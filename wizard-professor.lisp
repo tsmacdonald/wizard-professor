d6681e08a6c700cf8963767b7c88955bcf99328e
Tim Macdonald <tsmacdonald@gmail.com>
2013-10-27 14:15:55 -0500

    Clear edit-distance's memo table after each invocation

--- wizard-professor.lisp
+++ wizard-professor.lisp
@@ -5,30 +5,40 @@
 (defparameter *words*
   (with-open-file (in "/usr/share/dict/words")
     (loop for line = (read-line in nil) while line collect line)))
-  
-(fare-memoization:define-memo-function edit-distance
-    (source target &key
-	    (insert-cost 2) (delete-cost 2) (substitute-cost 3)
-	    (transpose-cost 3) (nop-cost 0))
+
+(defparameter *results* (make-hash-table :test 'equal))
+
+(defstruct edit-scores
+  (insert-cost 2)
+  (delete-cost 2)
+  (substitute-cost 3)
+  (transpose-cost 3)
+  (nop-cost 0))
+
+(fare-memoization:define-memo-function (memoized-edit-distance :table *results*) (source target scores)
   (cond
     ((zerop (length source))
-     (* (length target) insert-cost))
+     (* (length target) (edit-scores-insert-cost scores)))
     ((zerop (length target))
-     (* (length source) delete-cost))
+     (* (length source) (edit-scores-delete-cost scores)))
     ((string-equal source target)
-     (* (length target) nop-cost))
+     (* (length target) (edit-scores-nop-cost scores)))
     (t
      (reduce #'min
 	     (remove nil
 		     (list
-		      (+ insert-cost (edit-distance source (subseq target 1)))
-		      (+ delete-cost (edit-distance (subseq source 1) target))
-		      (+ substitute-cost (edit-distance (subseq source 1) (subseq target 1)))
+		      (+ (edit-scores-insert-cost scores) (memoized-edit-distance source (subseq target 1) scores))
+		      (+ (edit-scores-delete-cost scores) (memoized-edit-distance (subseq source 1) target scores))
+		      (+ (edit-scores-substitute-cost scores) (memoized-edit-distance (subseq source 1) (subseq target 1) scores))
 		      (when (and
 			     (>= (length source) 2)
 			     (>= (length target) 2)
 			     (char-equal (char source 0) (char target 1))
 			     (char-equal (char target 0) (char source 1)))
-			(+ transpose-cost (edit-distance (subseq source 2) (subseq target 2))))
+			(+ (edit-scores-transpose-cost scores) (memoized-edit-distance (subseq source 2) (subseq target 2) scores)))
 		      (when (char-equal (char source 0) (char target 0))
-			(+ nop-cost (edit-distance (subseq source 1) (subseq target 1))))))))))
+			(+ (edit-scores-nop-cost scores) (memoized-edit-distance (subseq source 1) (subseq target 1) scores)))))))))
+
+(defun edit-distance (source target &optional (scores (make-edit-scores)))
+  (clrhash *results*)
+  (memoized-edit-distance source target scores))
